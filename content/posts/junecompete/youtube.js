83638b2608c17e40f8d7f0c66d652c9cad755db4
Jonathan Grant <jonathanallengrant@gmail.com>
2023-06-27 18:26:13 -0400

    fix sizing of youtube

--- content/posts/junecompete/youtube.js
+++ content/posts/junecompete/youtube.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const YouTubeEmbed = ({ videoId }) => (
-  <iframe
-    width="560"
-    height="315"
-    src={`https://www.youtube.com/embed/${videoId}`}
-    frameborder="0"
-    allowfullscreen
-    title="YouTube video player"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-  ></iframe>
+  <div style={{ position: 'relative', paddingBottom: '35%', height: 0, overflow: 'hidden', maxWidth: '560px', maxHeight: '315px' }}>
+    <iframe
+      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 0 }}
+      src={`https://www.youtube.com/embed/${videoId}`}
+      allowFullScreen
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
+  </div>
 );
 
 const YouTubePlaylistEmbed = ({ playlistId }) => (
-  <iframe
-    width="560"
-    height="315"
-    src={`https://www.youtube.com/embed/videoseries?list=${playlistId}`}
-    frameborder="0"
-    allowfullscreen
-    title="YouTube video player"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-  ></iframe>
+  <div style={{ position: 'relative', paddingBottom: '35%', height: 0, overflow: 'hidden', maxWidth: '560px', maxHeight: '315px' }}>
+    <iframe
+      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 0 }}
+      src={`https://www.youtube.com/embed/videoseries?list=${playlistId}`}
+      allowfullscreen
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
+  </div>
 );
 
 export {YouTubeEmbed, YouTubePlaylistEmbed};
