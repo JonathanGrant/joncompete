66bd3bec0ffc6fa8a6a663b6d21f076749638295
Jonathan Grant <jonathanallengrant@gmail.com>
2023-10-17 16:35:53 -0400

    add lazy loading images

--- content/posts/junecompete/photos.js
+++ content/posts/junecompete/photos.js
@@ -5,7 +5,7 @@ const ImageGallery = ({ images }) => {
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', gap: '10px' }}>
       {images.map((image, index) => (
         <a key={index} style={{ textAlign: 'center' }} href={image.src} target="_blank">
-          <img src={image.src} alt={image.caption} style={{ width: '100%' }} />
+          <img src={image.src} alt={image.caption} loading="lazy" style={{ width: '100%' }} />
           <p style={{ fontSize: '14px' }}>{image.caption}</p>
         </a>
       ))}
